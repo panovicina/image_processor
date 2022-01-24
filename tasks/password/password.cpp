e50b73e0a1c55b42710cd06a10d7a9450c62c4cc
panovicina <panovicinanastya2004@gmail.com>
2022-01-24 19:36:58 +0300

    Password

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,5 +1,36 @@
 #include "password.h"
+#include <cctype>
 
 bool ValidatePassword(const std::string& password) {
-    return false;
+    int lower = 0;
+    int supper = 0;
+    int digit = 0;
+    int other = 0;
+    if (size(password) > 15 size(password) < 7) {
+        return false;
+    }
+    for (int i = 0; i < size(password); ++i) {
+        if (islower(password[i])) {
+            lower = 1;
+        }
+        if (isupper(password[i])) {
+            supper = 1;
+        }
+        if (isdigit(password[i])) {
+            digit = 1;
+        }
+        if (ispunct(password[i])) {
+            other = 1;
+        }
+
+        if ((int)password[i] > 126(int)password[i] < 33) {
+            return false;
+        }
+    }
+
+    if (lower + supper + digit + other < 3) {
+        return false;
+    }
+
+    return true;
 }
