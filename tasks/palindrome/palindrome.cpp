9c3e0912f10f22bdbe33b0990fff238a06c2d4ee
panovicina <panovicinanastya2004@gmail.com>
2022-01-24 17:54:08 +0300

    Palindrome

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,23 @@
 #include "palindrome.h"
+#include <string>
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    int i = 0;
+    int j = size(str) - 1;
+    while (i <= j) {
+        if (str[i] == ' ') {
+            ++i;
+            continue;
+        }
+        if (str[j] == ' ') {
+            --j;
+            continue;
+        }
+        if (str[i] != str[j])
+            return false;
+        ++i;
+        --j;
+    
+    }
+  return true;
 }
