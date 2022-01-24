086388fad71a5bf50dc2741062336b64e8f90746
panovicina <panovicinanastya2004@gmail.com>
2022-01-24 16:30:17 +0300

    Changing the function

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -1,5 +1,14 @@
 #include "associative_operation.h"
 
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
-    return false;
+    for (int x = 0; x < table.size(); ++x) {
+        for (int y = 0; y < table[x].size(); ++y) {
+            for (int z = 0; z < table.size(); ++z) {
+                if (table[table[x][y]][z] != table[x][table[y][z]]) {
+		    return false;
+		}
+            }
+        }
+    }
+    return true; 
 }
