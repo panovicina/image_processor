1fa1805a4a35dd82981a39e815cbc5d648051103
panovicina <panovicinanastya2004@gmail.com>
2022-01-24 19:02:43 +0300

    Associative operation 4.0

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -1,7 +1,7 @@
 #include "associative_operation.h"
 #include <vector>
 
-bool IsAssociative(const vector<std::vector<size_t>>& table) {
+bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
     for (int x = 0; x < table.size(); ++x) {
         for (int y = 0; y < table[x].size(); ++y) {
             for (int z = 0; z < table.size(); ++z) {
