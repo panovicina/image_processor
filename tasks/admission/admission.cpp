463c4b7f601c9ca5526d17e5972b2f9162838ed8
panovicina <panovicinanastya2004@gmail.com>
2022-02-07 00:00:54 +0300

    Adm

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -2,7 +2,9 @@
 #include<tuple>
 
 bool CompApp(const Applicant& a, const Applicant& b) {
-    return std::tie(a.points, b.student.birth_date.year, b.student.birth_date.month, b.student.birth_date.day, b.student.name) > std::tie(b.points, a.student.birth_date.year, a.student.birth_date.month, a.student.birth_date.day, a.student.name);
+    return std::tie(a.points, b.student.birth_date.year, b.student.birth_date.month,
+        b.student.birth_date.day, b.student.name) > std::tie(b.points, a.student.birth_date.year,
+            a.student.birth_date.month, a.student.birth_date.day, a.student.name);
 }
 AdmissionTable FillUniversities(const std::vector<University>& universities, const std::vector<Applicant>& applicants) {
     std::vector<Applicant> copy_applicants = applicants;
@@ -17,8 +19,10 @@ AdmissionTable FillUniversities(const std::vector<University>& universities, con
         for (size_t j = 0; j < copy_applicants[i].wish_list.size(); ++j) {
             if (!enter_uni) {
                 for (size_t k = 0; k < universities.size(); ++k) {
-                    if (result_list[universities[k].name].size() < universities[k].max_students && universities[k].name == copy_applicants[i].wish_list[j]) {
-                        result_list[copy_applicants[i].wish_list[j]].push_back(&applicants[applicant_indx[copy_applicants[i].student.name]].student);
+                    if (result_list[universities[k].name].size() < universities[k].max_students
+                        && universities[k].name == copy_applicants[i].wish_list[j]) {
+                        result_list[copy_applicants[i].wish_list[j]].push_back(
+                            &applicants[applicant_indx[copy_applicants[i].student.name]].student);
                         enter_uni = true;
                     }
                 }
