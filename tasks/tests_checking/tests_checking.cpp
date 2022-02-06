02eca53a4e6706eb6c8b73cfd02d46ff61aed294
panovicina <panovicinanastya2004@gmail.com>
2022-02-07 01:52:17 +0300

    Tests1

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -2,5 +2,22 @@
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::vector<std::string> result;
+    std::vector<std::string> students;
+    for (size_t i = 0; i < student_actions.size(); ++i) {
+        if (student_actions[i].side == Side::Bottom) {
+            students[i] = student_actions[i].name;
+        }
+        if (student_actions[i].side == Side::Top) {
+            students.resize(students.size() + 1);
+            for (size_t i = 0; i < students.size() - 1; ++i) {
+                students[i + 1] = students[i];
+            }
+            students[0] = student_actions[i].name;
+        }
+    }
+    for (size_t i = 0; i < queries.size(); ++i) {
+        result[i] = students[queries[i] - 1];
+    }
+    return result;
 }
