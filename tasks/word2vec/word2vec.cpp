383e8e31276326908fe9160f3aba903bb3f17d4b
panovicina <panovicinanastya2004@gmail.com>
2022-02-08 00:36:17 +0300

    Word

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -1,8 +1,26 @@
 #include "word2vec.h"
-
 #include <vector>
 
+int VecClose(std::vector<int>  v1, std::vector<int> v2) {
+    int res = 0;
+    for (int i = 0; i < v2.size(); ++i) {
+        res += v1[i] * v2[i];
+    }
+    return res;
+}
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
-    return {};
+    std::vector<std::string> Result;
+    int vc_max = 0;
+    for (int i = 1; i < vectors.size(); ++i) {
+        if (VecClose(vectors[0], vectors[i]) > vc_max) {
+            vc_max = VecClose(vectors[0], vectors[i]);
+        }
+    }
+    for (int i = 1; i < vectors.size(); ++i) {
+        if (VecClose(vectors[0], vectors[i]) == vc_max) {
+            Result.push_back(words[i]);
+        }
+    }
+    return Result;
 }
