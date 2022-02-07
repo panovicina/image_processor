acdc3093d2436de86bc0364cff3ab04a7d013e63
panovicina <panovicinanastya2004@gmail.com>
2022-02-07 20:58:31 +0300

    Test ch3

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,19 +1,18 @@
-#include "tests_checking.h"
+include "tests_checking.h"
 #include <deque>
 
-std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
-                                       const std::vector<size_t>& queries) {
+std::vector<std::string> StudentsOrder(const std::vector<StudentAction>&student_actions,
+                                       const std::vector<size_t>&queries) {
     std::vector<std::string> result;
     std::deque<std::string> students;
     for (size_t i = 0; i < student_actions.size(); ++i) {
-        switch (student_actions[i].side)
-        {
-        case Side::Top:
-            students.push_front(student_actions[i].name);
-        case Side::Bottom:
-            students.push_back(student_actions[i].name);
-        default:
-            break;
+        switch (student_actions[i].side) {
+            case Side::Top:
+                students.push_front(student_actions[i].name);
+            case Side::Bottom:
+                students.push_back(student_actions[i].name);
+            default:
+                break;
         }
     }
     for (size_t i = 0; i < queries.size(); ++i) {
