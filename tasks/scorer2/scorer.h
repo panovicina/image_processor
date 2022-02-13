16e0c6c7f8c0e8724b23eb1aa3844b9d23cb18b3
panovicina <panovicinanastya2004@gmail.com>
2022-02-13 21:35:51 +0300

    Scorer2.5

--- tasks/scorer2/scorer.h
+++ tasks/scorer2/scorer.h
@@ -5,38 +5,18 @@
 #include <string>
 #include <vector>
 
-void DelStudents(const StudentName& student_name) {
-    if (score_table[student_name].empty()) {
-        score_table.erase(student_name);
-    }
-}
-void DelFromTable(const StudentName& student_name, const TaskName& task_name) {
-    score_table[student_name].erase(task_name);
-    DelStudents(student_name);
-}
-void AddToTable(const StudentName& student_name, const TaskName& task_name) {
-    score_table[student_name].insert(task_name);
-}
+using StudentName = std::string;
+using TaskName = std::string;
+
+using ScoreTable = std::map<StudentName, std::set<TaskName>>;
 
 class Scorer {
 public:
-    ScoreTable score_table;
-    void OnCheckFailed(const StudentName& student_name, const TaskName& task_name) {
-        DelFromTable(student_name, task_name);
-    }
-    void OnCheckSuccess(const StudentName& student_name, const TaskName& task_name) {
-        AddToTable(student_name, task_name);
-    }
-    void OnMergeRequestOpen(const StudentName& student_name, const TaskName& task_name) {
-        DelFromTable(student_name, task_name);
-    }
-    void OnMergeRequestClosed(const StudentName& student_name, const TaskName& task_name) {
-        AddToTable(student_name, task_name);
-    }
-    void Reset() {
-        score_table.clear();
-    }
-    ScoreTable GetScoreTable() const {
-        return score_table;
-    }
-}
+    void OnCheckFailed(const StudentName& student_name, const TaskName& task_name);
+    void OnCheckSuccess(const StudentName& student_name, const TaskName& task_name);
+    void OnMergeRequestOpen(const StudentName& student_name, const TaskName& task_name);
+    void OnMergeRequestClosed(const StudentName& student_name, const TaskName& task_name);
+    void Reset();
+
+    ScoreTable GetScoreTable() const;
+};
