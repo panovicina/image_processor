3ed8f1dc9b349177f6001fb40efd349973b6436b
panovicina <panovicinanastya2004@gmail.com>
2022-02-13 21:45:57 +0300

    Scorer2.7

--- tasks/scorer2/scorer.h
+++ tasks/scorer2/scorer.h
@@ -9,14 +9,3 @@ using StudentName = std::string;
 using TaskName = std::string;
 
 using ScoreTable = std::map<StudentName, std::set<TaskName>>;
-
-class Scorer {
-public:
-    void OnCheckFailed(const StudentName& student_name, const TaskName& task_name);
-    void OnCheckSuccess(const StudentName& student_name, const TaskName& task_name);
-    void OnMergeRequestOpen(const StudentName& student_name, const TaskName& task_name);
-    void OnMergeRequestClosed(const StudentName& student_name, const TaskName& task_name);
-    void Reset();
-
-    ScoreTable GetScoreTable() const;
-};
