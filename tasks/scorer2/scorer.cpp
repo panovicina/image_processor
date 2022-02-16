67b99df5cb3d8499f8b251e6b05c6e435e52b82e
panovicina <panovicinanastya2004@gmail.com>
2022-02-16 20:44:07 +0300

    S6

--- tasks/scorer2/scorer.cpp
+++ tasks/scorer2/scorer.cpp
@@ -2,17 +2,17 @@
 
 using ScoreTable = std::map<StudentName, std::set<TaskName>>;
 ScoreTable score_table;
-std::map<std::pair<StudentName, TaskName>, EventType> merge;
-std::map<std::pair<StudentName, TaskName>, EventType> check;
-void DelStudents(const StudentName& student_name) {
+std::map<std::pair<StudentName, TaskName>, Scorer::EventType> merge;
+std::map<std::pair<StudentName, TaskName>, Scorer::EventType> check;
+void Scorer::DelStudents(const StudentName& student_name) {
     if (score_table[student_name].empty()) {
         score_table.erase(student_name);
     }
 }
-void Checking(const StudentName& student_name, const TaskName& task_name) {
-    EventType merge_condition = merge[std::make_pair(student_name, task_name)];
-    EventType check_condition = merge[std::make_pair(student_name, task_name)];
-    if (merge_condition == EventType::MergeRequestOpen || check_condition == EventType::CheckFailed) {
+void Scorer::Checking(const StudentName& student_name, const TaskName& task_name) {
+    Scorer::EventType merge_condition = merge[std::make_pair(student_name, task_name)];
+    Scorer::EventType check_condition = merge[std::make_pair(student_name, task_name)];
+    if (merge_condition == Scorer::EventType::MergeRequestOpen || check_condition == Scorer::EventType::CheckFailed) {
         score_table[student_name].erase(task_name);
         DelStudents(student_name);
     } else {
@@ -20,23 +20,23 @@ void Checking(const StudentName& student_name, const TaskName& task_name) {
     }
 }
 void Scorer::OnCheckFailed(const StudentName& student_name, const TaskName& task_name) {
-    EventType check_condition = merge[std::make_pair(student_name, task_name)];
-    check_condition = EventType::CheckFailed;
+    Scorer::EventType check_condition = merge[std::make_pair(student_name, task_name)];
+    check_condition = Scorer::EventType::CheckFailed;
     Checking(student_name, task_name);
 }
 void Scorer::OnCheckSuccess(const StudentName& student_name, const TaskName& task_name) {
-    EventType check_condition = merge[std::make_pair(student_name, task_name)];
-    check_condition = EventType::CheckSuccess;
+    Scorer::EventType check_condition = merge[std::make_pair(student_name, task_name)];
+    check_condition = Scorer::EventType::CheckSuccess;
     Checking(student_name, task_name);
 }
 void Scorer::OnMergeRequestOpen(const StudentName& student_name, const TaskName& task_name) {
-    EventType merge_condition = merge[std::make_pair(student_name, task_name)];
-    merge_condition = EventType::MergeRequestOpen;
+    Scorer::EventType merge_condition = merge[std::make_pair(student_name, task_name)];
+    merge_condition = Scorer::EventType::MergeRequestOpen;
     Checking(student_name, task_name);
 }
 void Scorer::OnMergeRequestClosed(const StudentName& student_name, const TaskName& task_name) {
-    EventType merge_condition = merge[std::make_pair(student_name, task_name)];
-    merge_condition = EventType::MergeRequestClosed;
+    Scorer::EventType merge_condition = merge[std::make_pair(student_name, task_name)];
+    merge_condition = Scorer::EventType::MergeRequestClosed;
     Checking(student_name, task_name);
 }
 void Scorer::Reset() {
