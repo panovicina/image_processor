a1dd3de52aec478b8e407b02dd7f1826a52b20aa
Dmitry Levanov <levanovd@gmail.com>
2022-02-16 23:22:32 +0300

    Set asan default options

--- contrib/catch/catch.hpp
+++ contrib/catch/catch.hpp
@@ -46,6 +46,22 @@
 #    pragma GCC diagnostic ignored "-Wpadded"
 #endif
 // end catch_suppress_warnings.h
+
+// start ignore out of memory errors in ASAN
+#ifndef __has_feature
+#define __has_feature(feature) 0
+#endif
+
+#if __has_feature(address_sanitizer) || defined(__SANITIZE_ADDRESS__)
+#ifdef __cplusplus
+extern "C"
+#endif
+const char* __asan_default_options() {
+  return "allocator_may_return_null=1";
+}
+#endif
+// end ignore out of memory errors in ASAN
+
 #if defined(CATCH_CONFIG_MAIN) || defined(CATCH_CONFIG_RUNNER)
 #  define CATCH_IMPL
 #  define CATCH_CONFIG_ALL_PARTS
