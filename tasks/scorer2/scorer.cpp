d8666449c78859eae39137d6126958d84e59804a
panovicina <panovicinanastya2004@gmail.com>
2022-02-16 21:01:20 +0300

    S8

--- tasks/scorer2/scorer.cpp
+++ tasks/scorer2/scorer.cpp
@@ -11,7 +11,7 @@ void Scorer::DelStudents(const StudentName& student_name) {
 }
 void Scorer::Checking(const StudentName& student_name, const TaskName& task_name) {
     Scorer::EventType merge_condition = merge[std::make_pair(student_name, task_name)];
-    Scorer::EventType check_condition = merge[std::make_pair(student_name, task_name)];
+    Scorer::EventType check_condition = check[std::make_pair(student_name, task_name)];
     if (merge_condition == Scorer::EventType::MergeRequestOpen || check_condition == Scorer::EventType::CheckFailed) {
         score_table[student_name].erase(task_name);
         DelStudents(student_name);
