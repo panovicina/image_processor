037c9e8aa4f23466f3998e705ae39b752616b480
panovicina <panovicinanastya2004@gmail.com>
2022-02-16 21:21:34 +0300

    S9

--- tasks/scorer2/scorer.cpp
+++ tasks/scorer2/scorer.cpp
@@ -9,35 +9,24 @@ void Scorer::DelStudents(const StudentName& student_name) {
         score_table.erase(student_name);
     }
 }
-void Scorer::Checking(const StudentName& student_name, const TaskName& task_name) {
-    Scorer::EventType merge_condition = merge[std::make_pair(student_name, task_name)];
-    Scorer::EventType check_condition = check[std::make_pair(student_name, task_name)];
-    if (merge_condition == Scorer::EventType::MergeRequestOpen || check_condition == Scorer::EventType::CheckFailed) {
-        score_table[student_name].erase(task_name);
-        DelStudents(student_name);
-    } else {
-        score_table[student_name].insert(task_name);
-    }
+void Scorer::DelFromTable(const StudentName& student_name, const TaskName& task_name) {
+    score_table[student_name].erase(task_name);
+    DelStudents(student_name);
+}
+void Scorer::AddToTable(const StudentName& student_name, const TaskName& task_name) {
+    score_table[student_name].insert(task_name);
 }
 void Scorer::OnCheckFailed(const StudentName& student_name, const TaskName& task_name) {
-    Scorer::EventType check_condition = check[std::make_pair(student_name, task_name)];
-    check_condition = Scorer::EventType::CheckFailed;
-    Checking(student_name, task_name);
+    DelFromTable(student_name, task_name);
 }
 void Scorer::OnCheckSuccess(const StudentName& student_name, const TaskName& task_name) {
-    Scorer::EventType check_condition = check[std::make_pair(student_name, task_name)];
-    check_condition = Scorer::EventType::CheckSuccess;
-    Checking(student_name, task_name);
+    AddToTable(student_name, task_name);
 }
 void Scorer::OnMergeRequestOpen(const StudentName& student_name, const TaskName& task_name) {
-    Scorer::EventType merge_condition = merge[std::make_pair(student_name, task_name)];
-    merge_condition = Scorer::EventType::MergeRequestOpen;
-    Checking(student_name, task_name);
+    DelFromTable(student_name, task_name);
 }
 void Scorer::OnMergeRequestClosed(const StudentName& student_name, const TaskName& task_name) {
-    Scorer::EventType merge_condition = merge[std::make_pair(student_name, task_name)];
-    merge_condition = Scorer::EventType::MergeRequestClosed;
-    Checking(student_name, task_name);
+    AddToTable(student_name, task_name);
 }
 void Scorer::Reset() {
     score_table.clear();
--- tasks/scorer2/scorer.h
+++ tasks/scorer2/scorer.h
@@ -18,13 +18,14 @@ public:
         MergeRequestOpen,
         MergeRequestClosed,
     };
+    void DelStudents(const StudentName& student_name);
+    void DelFromTable(const StudentName& student_name, const TaskName& task_name);
     void OnCheckFailed(const StudentName& student_name, const TaskName& task_name);
     void OnCheckSuccess(const StudentName& student_name, const TaskName& task_name);
     void OnMergeRequestOpen(const StudentName& student_name, const TaskName& task_name);
     void OnMergeRequestClosed(const StudentName& student_name, const TaskName& task_name);
     void Reset();
-    void DelStudents(const StudentName& student_name);
-    void Checking(const StudentName& student_name, const TaskName& task_name);
+    void AddToTable(const StudentName& student_name, const TaskName& task_name);
 
     ScoreTable GetScoreTable() const;
 };
