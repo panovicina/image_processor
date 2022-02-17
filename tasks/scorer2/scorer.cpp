4d220039c95e2f068e02572615aae8c8a24153df
panovicina <panovicinanastya2004@gmail.com>
2022-02-17 18:24:34 +0300

    S13

--- tasks/scorer2/scorer.cpp
+++ tasks/scorer2/scorer.cpp
@@ -5,24 +5,6 @@ ScoreTable score_table;
 std::map<std::pair<StudentName, TaskName>, Scorer::EventType> merge;
 std::map<std::pair<StudentName, TaskName>, Scorer::EventType> check;
 std::set<TaskName> set;
-void Scorer::DelStudents(const StudentName& student_name) {
-    if (score_table[student_name].empty()) {
-        score_table.erase(student_name);
-    }
-}
-void Scorer::DelFromTable(const StudentName& student_name, const TaskName& task_name) {
-    score_table[student_name].erase(task_name);
-    DelStudents(student_name);
-}
-void Scorer::AddToTable(const StudentName& student_name, const TaskName& task_name) {
-    if (score_table.find(student_name) == score_table.end()) {
-        set.insert(task_name);
-        score_table.insert({student_name, set});
-    } else {
-        score_table[student_name].insert(task_name);
-    }
-    set.clear();
-}
 void Scorer::OnCheckFailed(const StudentName& student_name, const TaskName& task_name) {
     DelFromTable(student_name, task_name);
 }
