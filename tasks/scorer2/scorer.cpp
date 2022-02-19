ed97317df45de5b0a742c93adc56774a24f2ec56
panovicina <panovicinanastya2004@gmail.com>
2022-02-20 02:27:08 +0300

    Scorrer6

--- tasks/scorer2/scorer.cpp
+++ tasks/scorer2/scorer.cpp
@@ -12,8 +12,8 @@ void Scorer::OnCheckFailed(const StudentName& student_name, const TaskName& task
     }
 }
 void Scorer::OnCheckSuccess(const StudentName& student_name, const TaskName& task_name) {
-    if (was_merge_open[student_name].find(task_name) == was_merge_open[student_name].end() || 
-    was_merge_open.find(student_name) == was_merge_open.end()) {
+    if (was_merge_open[student_name].find(task_name) == was_merge_open[student_name].end() ||
+        was_merge_open.find(student_name) == was_merge_open.end()) {
         AddToTable(student_name, task_name);
     }
     if (was_failed.find(student_name) != was_failed.end()) {
@@ -31,8 +31,8 @@ void Scorer::OnMergeRequestOpen(const StudentName& student_name, const TaskName&
     }
 }
 void Scorer::OnMergeRequestClosed(const StudentName& student_name, const TaskName& task_name) {
-    if (was_failed[student_name].find(task_name) == was_failed[student_name].end() || 
-    was_failed.find(student_name) == was_failed.end()) {
+    if (was_failed[student_name].find(task_name) == was_failed[student_name].end() ||
+        was_failed.find(student_name) == was_failed.end()) {
         AddToTable(student_name, task_name);
     }
     if (was_merge_open.find(student_name) != was_merge_open.end()) {
